dc863d6ee76fa5041277d5d98e729df60ca1a046
kegilko <nomads_kei@hotmail.com>
2015-06-08 04:34:16 -0400

    first commit

---
+++ sites/default/files/php/twig/1#6a#78#2366047176580fd9b0bc42cf376f2fdb12af0444354ebbe13557f4883547/5afd1b5af25e7f31669c0cca6dd0d067c1ae017840b9709dcbc4960cf0b147c5.php
@@ -0,0 +1,41 @@
+<?php
+
+/* core/themes/classy/templates/form/checkboxes.html.twig */
+class __TwigTemplate_6a782366047176580fd9b0bc42cf376f2fdb12af0444354ebbe13557f4883547 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 15
+        echo "<div";
+        echo $this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["attributes"]) ? $context["attributes"] : null), "addClass", array(0 => "form-checkboxes"), "method"), "html", null, true);
+        echo ">";
+        echo $this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["children"]) ? $context["children"] : null), "html", null, true);
+        echo "</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/form/checkboxes.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 15,);
+    }
+}
