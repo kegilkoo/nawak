d27e5b5f3f8fb3efe35e0d53f5bc017f8f160051
kegilko <nomads_kei@hotmail.com>
2015-06-21 16:30:14 +0200

    add new custom skeleton module

---
+++ modules/custom/raspberries/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\raspberries\Tests\DefaultController.
+ */
+
+namespace Drupal\raspberries\Tests;
+
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Provides automated tests for the raspberries module.
+ */
+class DefaultControllerTest extends WebTestBase {
+  /**
+   * {@inheritdoc}
+   */
+  public static function getInfo() {
+    return array(
+      'name' => "raspberries DefaultController's controller functionality",
+      'description' => 'Test Unit for module raspberries and controller DefaultController.',
+      'group' => 'Other',
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+  }
+
+  /**
+   * Tests raspberries functionality.
+   */
+  public function testDefaultController() {
+    // Check that the basic functions of module raspberries.
+    $this->assertEqual(TRUE, TRUE, 'Test Unit Generated via App Console.');
+  }
+
+}
